f1c18efe1d5debb7e5f8e381e4e92a44b635f421
priyanshi160 <upadhyaypriyanshi384@gmail.com>
2024-05-17 15:59:43 +0530

    Updated files

---
+++ Responsive Portfolio Website/script.js
@@ -0,0 +1,92 @@
+$(document).ready(function(){
+    $(window).scroll(function(){
+        // sticky navbar on scroll script
+        if(this.scrollY > 20){
+            $('.navbar').addClass("sticky");
+        }else{
+            $('.navbar').removeClass("sticky");
+        }
+        
+        // scroll-up button show/hide script
+        if(this.scrollY > 500){
+            $('.scroll-up-btn').addClass("show");
+        }else{
+            $('.scroll-up-btn').removeClass("show");
+        }
+    });
+
+    // slide-up script
+    $('.scroll-up-btn').click(function(){
+        $('html').animate({scrollTop: 0});
+        // removing smooth scroll on slide-up button click
+        $('html').css("scrollBehavior", "auto");
+    });
+
+    $('.navbar .menu li a').click(function(){
+        // applying again smooth scroll on menu items click
+        $('html').css("scrollBehavior", "smooth");
+    });
+
+    // toggle menu/navbar script
+    $('.menu-btn').click(function(){
+        $('.navbar .menu').toggleClass("active");
+        $('.menu-btn i').toggleClass("active");
+    });
+
+    // typing text animation script
+    var typed = new Typed(".typing", {
+        strings: ["Web Developer", "Blogger", "Designer", "Freelancer"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
+    });
+
+    var typed = new Typed(".typing-2", {
+        strings: ["Web Developer", "Blogger", "Designer", "Freelancer"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
+    });
+
+    // owl carousel script
+    $('.carousel').owlCarousel({
+        margin: 20,
+        loop: true,
+        autoplay: true,
+        autoplayTimeOut: 2000,
+        autoplayHoverPause: true,
+        responsive: {
+            0:{
+                items: 1,
+                nav: false
+            },
+            600:{
+                items: 2,
+                nav: false
+            },
+            1000:{
+                items: 3,
+                nav: false
+            }
+        }
+    });
+
+    
+    function sendEmail(){
+
+
+
+    Email.send({
+        secureToken : "26594670-f048-4db4-8304-1a3c10b4ae9b",
+        To : 'work.priyanshiu@gmail.com',
+        From : document.getElementById("email").value,
+        Subject : "New contact Enquiry",
+        Body : "Name: " + document.getElementById("name").value + 
+        "<br> Email: " + document.getElementById("email").value +
+        "<br> Phone: " + document.getElementById("phone").value +
+        "<br> Message: " + document.getElementById("message").value
+ }).then(
+      message => alert(message)
+    );
+}
+});
